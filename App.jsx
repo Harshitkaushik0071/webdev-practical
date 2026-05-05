9400a9dfe62afaa73e6025122184564ab1e3d8e5
Hrashit Kaushik <82391810+Harshitkaushik0071@users.noreply.github.com>
2026-05-05 10:32:46 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,10 +1,32 @@
-import Profile from "./components/Profile";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div>
-      <h1>React Context API Example</h1>
-      <Profile />
+      <h1>Posts Data</h1>
+
+      {posts.map((post) => (
+        <div
+          key={post.id}
+          style={{
+            border: "1px solid black",
+            margin: "10px",
+            padding: "10px",
+          }}
+        >
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ))}
     </div>
   );
 }
